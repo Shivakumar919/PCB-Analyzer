eac3c5acf6ba5c033a1214c745835f0b8066fa32
Shivakumar919 <shivakumar91999@gmail.com>
2026-07-01 19:03:56 +0530

    Fix layoutjsx  import for Netlify

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 import pcbImg from "../assets/loginsignupimg.png";
 import { login } from "../services/authservice.js";
 
